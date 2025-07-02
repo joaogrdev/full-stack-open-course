f9ddf1e0e4dfe64e03a30c43ce87111e71add546
joao.sousa <joao.sousa@thinklife.com.br>
2025-07-02 15:04:58 -0300

    feat: exercise 3.11

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
-const cors = require("cors");
 const app = express();
-
-app.use(cors()); 
 
 app.use(express.json());
 
@@ -12,6 +9,8 @@ morgan.token("body", (req, res) => JSON.stringify(req.body));
 app.use(
   morgan(":method :url :status :res[content-length] - :response-time ms:body")
 );
+
+app.use(express.static('dist'))
 
 let persons = [
   {
